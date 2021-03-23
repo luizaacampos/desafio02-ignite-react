6ed0dd94a30e891425efe577f6352c684b2ec1c1
Luiza Campos <luiza.almcampos@gmail.com>
2021-03-23 18:14:51 -0300

    funcional, 5 testes falhando

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -1,7 +1,7 @@
 import { createContext, ReactNode, useContext, useState } from 'react';
 import { toast } from 'react-toastify';
 import { api } from '../services/api';
-import { Product, Stock } from '../types';
+import { Product } from '../types';
 
 interface CartProviderProps {
   children: ReactNode;
@@ -23,28 +23,49 @@ const CartContext = createContext<CartContextData>({} as CartContextData);
 
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const [cart, setCart] = useState<Product[]>(() => {
-    // const storagedCart = Buscar dados do localStorage
+    const storagedCart = localStorage.getItem('@RocketShoes:cart')
 
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
+    if (storagedCart) {
+      return JSON.parse(storagedCart);
+    }
 
     return [];
   });
 
   const addProduct = async (productId: number) => {
     try {
-      // TODO
+      const { data: product } = await api.get(`/products/${productId}`)
+      const { data: Stock } = await api.get(`/stock/${product.id}`)
+
+      const cartIndex = cart.findIndex((cart) => cart.id === productId)
+      if (cartIndex === -1) {
+        if (Stock.amount > 0) {
+          const newCart = [...cart, {...product, amount: 1}]
+          setCart(newCart)
+        } else {
+          toast.error('Quantidade solicitada fora de estoque');
+        }
+      } else {
+        updateProductAmount({ productId, amount: cart[cartIndex].amount + 1})
+      }
+      localStorage.setItem('@RocketShoes:cart', JSON.stringify(cart))
     } catch {
-      // TODO
+      toast.error('Erro na adição do produto');
     }
   };
 
   const removeProduct = (productId: number) => {
     try {
-      // TODO
+      let isOnCart = cart.findIndex((product) => product.id === productId)
+      if (isOnCart === -1) throw new Error('Erro na remoção do produto')
+      
+      const filteredCart = cart.filter(product => product.id !== productId)
+      
+      setCart(filteredCart)
+
+      localStorage.setItem('@RocketShoes:cart', JSON.stringify(filteredCart))
     } catch {
-      // TODO
+      toast.error('Erro na remoção do produto');
     }
   };
 
@@ -53,9 +74,28 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
     amount,
   }: UpdateProductAmount) => {
     try {
-      // TODO
+      const productDataStock = await (await api.get(`stock/${productId}`)).data
+     
+      if (amount < 1) return
+      
+      if (amount > productDataStock.amount) {
+        toast.error('Quantidade solicitada fora de estoque')
+        return 
+      }
+        
+      const newCart = cart.map((product) => {
+        if (product.id === productId) {
+          return {
+            ...product,
+            amount
+          }
+        }
+          return product
+      })
+        setCart(newCart)
+      localStorage.setItem('@RocketShoes:cart', JSON.stringify(cart))
     } catch {
-      // TODO
+      toast.error('Erro na alteração de quantidade do produto')
     }
   };
 
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,6 +5,7 @@ import { ProductList } from './styles';
 import { api } from '../../services/api';
 import { formatPrice } from '../../util/format';
 import { useCart } from '../../hooks/useCart';
+import Cart from '../Cart';
 
 interface Product {
   id: number;
@@ -22,46 +23,67 @@ interface CartItemsAmount {
 }
 
 const Home = (): JSX.Element => {
-  // const [products, setProducts] = useState<ProductFormatted[]>([]);
-  // const { addProduct, cart } = useCart();
+  const [products, setProducts] = useState<ProductFormatted[]>([]);
+  const { addProduct, cart } = useCart();
+  const [unformatedProducts, setUnformatedProducts] = useState<Product[]>([])
 
-  // const cartItemsAmount = cart.reduce((sumAmount, product) => {
-  //   // TODO
-  // }, {} as CartItemsAmount)
+  const cartItemsAmount = cart.reduce((sumAmount, product) => {
+    sumAmount[product.id] = product.amount
+    return sumAmount
+  }, {} as CartItemsAmount)
 
   useEffect(() => {
     async function loadProducts() {
-      // TODO
+      const { data } = await api.get('products')
+      
+      const formatingData = data.map((product: ProductFormatted) => {
+        return {
+          ...product,
+          priceFormatted: formatPrice(product.price),
+        }
+      })
+
+      setProducts(formatingData)
+
+      console.log(products)
     }
 
     loadProducts();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   function handleAddProduct(id: number) {
-    // TODO
+
+    if (products.filter(product => product.id === id)) {
+      addProduct(id)
+    }
   }
 
   return (
     <ProductList>
-      <li>
-        <img src="https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg" alt="Tênis de Caminhada Leve Confortável" />
-        <strong>Tênis de Caminhada Leve Confortável</strong>
-        <span>R$ 179,90</span>
-        <button
-          type="button"
-          data-testid="add-product-button"
-        // onClick={() => handleAddProduct(product.id)}
-        >
-          <div data-testid="cart-product-quantity">
-            <MdAddShoppingCart size={16} color="#FFF" />
-            {/* {cartItemsAmount[product.id] || 0} */} 2
-          </div>
+      {products.map(product => {
+        return (
+            <li key={product.id}>
+              <img src={product.image} alt={product.title} />
+              <strong>{product.title}</strong>
+              <span>{product.priceFormatted}</span>  
+              <button
+                type="button"
+                data-testid="add-product-button"
+                onClick={() => handleAddProduct(product.id)}
+                >
+                <div data-testid="cart-product-quantity">
+                  <MdAddShoppingCart size={16} color="#FFF" />
+                  {cartItemsAmount[product.id] || 0} 
+                </div>
 
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
+                <span>ADICIONAR AO CARRINHO</span>
+              </button>
+            </li>
+        )
+      })}
     </ProductList>
-  );
+  )
 };
 
 export default Home;
